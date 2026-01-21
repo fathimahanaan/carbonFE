20e7918f0343c6c665946dc27cc586774ae4fad5
fathimahanaan <hanaanfh764@icloud.com>
2026-01-21 20:38:30 +0000

    food options

---
+++ src/hooks/food/useGetAllFood.jsx
@@ -0,0 +1,40 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { toast } from "react-toastify";
+import { base_url } from "../../utils/constants";
+
+export const useGetAllFood = () => {
+  const [options, setOptions] = useState({
+    activities: [],
+  });
+  const [loading, setLoading] = useState(false);
+
+  const getFoodOptions = async () => {
+    setLoading(true);
+    try {
+      const res = await axios.get(`${base_url}/food/options`, {
+        withCredentials: true,
+      });
+
+      setOptions({
+        activities: res.data.activities,
+      });
+    } catch (err) {
+      toast.error(
+        err?.response?.data?.message ||
+          err?.message ||
+          "Failed to load options"
+      );
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getFoodOptions();
+  }, []);
+
+  return { options, loading, getFoodOptions };
+};
+
+export default useGetAllFood;
